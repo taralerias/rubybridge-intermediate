d2364b79cf3a562c3d332f3aebf298489f49017a
Tara Lerias <tlerias@gmail.com>
2013-02-02 21:05:32 -0700

    Intermediate filesand devise

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Intermediate::Application.config.secret_token = '70e085aac0a5b8e7e41cb5287a8037e0a2361559f93dec13014996ee35db78ce6da85896eba991aac824259139254129f8e0f1c0e319890c134af33d1980cd69'
